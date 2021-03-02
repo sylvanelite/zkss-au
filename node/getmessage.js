2a6a5092c8227490254dfbd2115d51a44cc5b9b3
sylvanelite <stephen.pearson@sra.com.au>
2021-03-02 14:17:02 +0930

    adding dummy node server code

--- node/getmessage.js
+++ node/getmessage.js
@@ -1,4 +1,35 @@
 //get message
 //TODO: connect to DB or something...
 
+export default function() {
+    let res = "this is some code";
+    try{
+        
+	if(request.query.hasOwnProperty("api")){
+        res += "param not found";
+	}else{        
+		var api = request.query.api;
+		pool.connect().then(function(client){
+			return client.query(' SELECT key FROM api WHERE (key=$1) ',[api])
+			.then(function(result){
+                res+= (JSON.stringify(result));
+		 
+         
+		  }).catch(function(err) {
+			  console.log(err);
+			  client.release();
+			  responseObj.success=false;
+			  responseObj.data="Error API lookup";
+			res+= (JSON.stringify(responseObj));
+		  });
+		}).catch(function(err){
+			console.log(err);
+			responseObj.success=false;
+			responseObj.data="Error Connecting to DB";
+			res+= (JSON.stringify(responseObj));
+		});
+    }       
+    }catch(e){res+="error: "+e;}
+  return res;
+}
 
