16ff94fbc75fbe971defc68fca14d222933c3c27
Sylvan <sylvanelite@hotmail.com>
2021-03-02 19:23:58 +0930

    typo in param

--- node/getmessage.mjs
+++ node/getmessage.mjs
@@ -7,23 +7,17 @@ export default function(request,response,client) {
 						response.send("param not found");
 			}else{        
 				var api = request.query.api;
-				pool.connect().then(function(client){
-					return client.query(' SELECT key FROM api WHERE (key=$1) ',[api])
-					.then(function(result){
-										response.send(JSON.stringify(result));
-					}).catch(function(err) {
-						console.log(err);
-						client.release();
-						responseObj.success=false;
-						responseObj.data="Error API lookup";
-					response.send(JSON.stringify(responseObj));
-					});
-				}).catch(function(err){
-					console.log(err);
-					responseObj.success=false;
-					responseObj.data="Error Connecting to DB";
-					response.send(JSON.stringify(responseObj));
-				});
+                client.connect();
+                client.query(' SELECT key FROM api WHERE (key=$1) ',[api])
+                .then(function(result){
+                                    response.send(JSON.stringify(result));
+                }).catch(function(err) {
+                    console.log(err);
+                    client.release();
+                    responseObj.success=false;
+                    responseObj.data="Error API lookup";
+                response.send(JSON.stringify(responseObj));
+                });
 			}       
     }catch(e){
 			response.send("error: "+e);
