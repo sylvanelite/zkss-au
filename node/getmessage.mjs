cf3ec9fbb77cf9f3049cfc813a14c2044b46850c
sylvanelite <stephen.pearson@sra.com.au>
2021-03-03 08:23:55 +0930

    responseobj undefined

--- node/getmessage.mjs
+++ node/getmessage.mjs
@@ -1,26 +1,31 @@
 //get message
-//TODO: connect to DB or something...
 
 export default function(request,response,client) {
+	var responseObj = {
+		success:false,
+		data:{}
+	};
     try{
 			if(!request.query.hasOwnProperty("api")){
-						response.send("param not found");
+                response.send("param not found");
 			}else{        
 				var api = request.query.api;
                 client.connect();
                 client.query(' SELECT key FROM api WHERE (key=$1) ',[api])
                 .then(function(result){
-                                    response.send(JSON.stringify(result));
+                    responseObj.success = true;
+                    responseObj.data = JSON.stringify(result);
+                    response.send(responseObj);
                 }).catch(function(err) {
                     console.log(err);
                     client.release();
-                    responseObj.success=false;
                     responseObj.data="Error API lookup";
                 response.send(JSON.stringify(responseObj));
                 });
 			}       
     }catch(e){
-			response.send("error: "+e);
-		}
+        responseObj.data ="error: "+e;
+        response.send(responseObj);
+    }
 }
 
