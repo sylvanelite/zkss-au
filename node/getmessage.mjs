2ac89d76b076de0cc6357192a361bb9fdf2b884f
sylvanelite <stephen.pearson@sra.com.au>
2021-03-02 15:25:20 +0930

    import js

--- node/getmessage.mjs
+++ node/getmessage.mjs
@@ -1,35 +1,32 @@
 //get message
 //TODO: connect to DB or something...
 
-export default function(request,pool) {
-    let res = "this is some code";
+export default function(request,response,pool) {
     try{
-        
-	if(request.query.hasOwnProperty("api")){
-        res += "param not found";
-	}else{        
-		var api = request.query.api;
-		pool.connect().then(function(client){
-			return client.query(' SELECT key FROM api WHERE (key=$1) ',[api])
-			.then(function(result){
-                res+= (JSON.stringify(result));
-		 
-         
-		  }).catch(function(err) {
-			  console.log(err);
-			  client.release();
-			  responseObj.success=false;
-			  responseObj.data="Error API lookup";
-			res+= (JSON.stringify(responseObj));
-		  });
-		}).catch(function(err){
-			console.log(err);
-			responseObj.success=false;
-			responseObj.data="Error Connecting to DB";
-			res+= (JSON.stringify(responseObj));
-		});
-    }       
-    }catch(e){res+="error: "+e;}
-  return res;
+			if(!request.query.hasOwnProperty("api")){
+						rresponse.send("param not found");
+			}else{        
+				var api = request.query.api;
+				pool.connect().then(function(client){
+					return client.query(' SELECT key FROM api WHERE (key=$1) ',[api])
+					.then(function(result){
+										response.send(JSON.stringify(result));
+					}).catch(function(err) {
+						console.log(err);
+						client.release();
+						responseObj.success=false;
+						responseObj.data="Error API lookup";
+					response.send(JSON.stringify(responseObj));
+					});
+				}).catch(function(err){
+					console.log(err);
+					responseObj.success=false;
+					responseObj.data="Error Connecting to DB";
+					response.send(JSON.stringify(responseObj));
+				});
+			}       
+    }catch(e){
+			response.send("error: "+e);
+		}
 }
 
